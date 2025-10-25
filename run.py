bc6bec2266932fb89a2ab9322aa8994781e8b9de
Kevin Ferrucci <m47h_0@protonmail.com>
2025-10-24 22:54:21 -0500

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,20 @@
 from app import create_app
+from flask import Flask
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.exporter.azure.monitor.trace import AzureMonitorTraceExporter
+import os
+
+FlaskInstrumentor().instrument_app(app)
+
+if "APPLICATIONINSIGHTS_CONNECTION_STRING" in os.environ:
+    provider = TracerProvider()
+    exporter = AzureMonitorTraceExporter.from_connection_string(
+        os.environ["APPLICATIONINSIGHTS_CONNECTION_STRING"]
+    )
+    provider.add_span_processor(BatchSpanProcessor(exporter))
+
 
 app = create_app()
 
